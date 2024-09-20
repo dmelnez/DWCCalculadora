7676316ed8b69068c04f3d26277a556841c44134
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-09-20 09:42:06 +0200

    Update javascript.js

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -2,11 +2,11 @@ function calculoOperadores(){
 
     event.preventDefault(); // Evita que la pagina deba ser recargada
 
-    var primerNumero = document.getElementById('primerNumero').value
+    let primerNumero = document.getElementById('primerNumero').value
 
-    var segundoNumero = document.getElementById('segundoNumero').value
+    let segundoNumero = document.getElementById('segundoNumero').value
 
-    var operador = document.getElementById('operador').value
+    let operador = document.getElementById('operador').value
 
     if(operador != "+" && "-" && "*" && "/"){
         alert("Se ha producido un error")
